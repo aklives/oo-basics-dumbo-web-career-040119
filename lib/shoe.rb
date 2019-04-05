fb1789e5adbd71d791937d7a8288eda9707544dd
Adam Kearney <aklives@hotmail.com>
2019-04-05 01:08:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,15 @@
 class Shoe
   
+attr_accessor :brand, :color, :size, :material, :condition
+
 def initialize brand
   @brand = brand
+end
+
+def cobble
+  puts "The shoe has been repaired."
+  @condition = "new"
+end
 
 end
+
